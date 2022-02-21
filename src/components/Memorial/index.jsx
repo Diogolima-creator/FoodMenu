1ba7e6fe7056c6daaaaea496b8e2cc361ba5c484
Diogolima <Dbznetudo@gmail.com>
2022-02-21 08:48:05 -0400

    update memorial

--- src/components/Memorial/index.jsx
+++ src/components/Memorial/index.jsx
@@ -2,7 +2,7 @@ import * as C from './styles';
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
-import { testimonialList } from '../../data/TestimonialList';
+import { testimonialList } from '../../data/testimonialList';
 import { MemorialCard } from '../MemorialCard';
 
 export const Memorial = () => {
